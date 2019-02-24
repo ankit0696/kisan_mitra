fb99cd02bba7b61e340a09625161b4cc9ce8524f
Ankit Kumar <ankitkr0696@gmail.com>
2019-02-25 01:08:36 +0530

    up

--- lite.php
+++ lite.php
@@ -6,23 +6,249 @@
   <link href="css/lite.css" rel="stylesheet">
   <title>India Map</title>
  
- <!--  Scripts  -->
- <script type="text/javascript" src="lite/js/d3.min.js"></script>
- <script type="text/javascript" src="lite/js/d3.geo.min.js"></script>
+
 
  <!--  Styles  -->  
  <link type="text/css" rel="stylesheet" href="lite/css/index.css"/>
- <style type="text/css">
 
    
- #india {
-   fill: steelblue;
-   opacity: .8;
-   stroke: #000000;
-   stroke-width: .7;
-   }
- </style>
-  
+
+
+    <!-- Step 1 - Including the fusioncharts core library -->
+    <script type="text/javascript" src="http://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
+    <!-- Step 2 - Including the map renderer file -->
+    <script type="text/javascript" src="http://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.maps.js "></script>
+    <!-- Step 3 - Including the map definition file -->
+    <script type="text/javascript" src="http://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.maps/india.js"></script>
+    <!-- Step 4 - Including the fusion theme -->
+    <script type="text/javascript" src="http://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.theme.fusion.js"></script>
+
+    <script>
+    FusionCharts.ready(function() {
+            var annualPopulation = new FusionCharts({
+                "type": "maps/india",
+                "renderAt": "chart-container",
+                "width": "900",
+                "height": "600",
+                "dataFormat": "json",
+                "dataSource": {
+                    // Map Configuration
+                    "chart": {
+                        "caption": "Average Annual Population Growth",
+                        "subcaption": " 1955-2015",
+                        "numbersuffix": "%",
+                        "includevalueinlabels": "1",
+                        "labelsepchar": ": ",
+                        "entityFillHoverColor": "#FFF9C4",
+                        "theme": "fusion"
+                    },
+                    // Aesthetics; ranges synced with the slider
+                    "colorrange": {
+                        "minvalue": "0",
+                        "code": "#ffffff",
+                        "gradient": "1",
+                        "color": [{
+                            "minvalue": "0.0",
+                            "maxvalue": "33.3",
+                            "color": "#8bc9a0"
+                        }, {
+                            "minvalue": "33.3",
+                            "maxvalue": "66.6",
+                            "color": "#6ac789"
+                        }, {
+                            "minvalue": "66.6",
+                            "maxvalue": "100.0",
+                            "color": "#55c57a"
+                        }]
+                    },
+                    // Source data as JSON --> id represents countries of world.
+                    "data":[
+        {
+            "id": "001",
+            "value": "70",
+            "showLabel": "1"
+        },
+        {
+            "id": "002",
+            "value": "87",
+            "showLabel": "1"
+        },
+        {
+            "id": "003",
+            "value": "75",
+            "showLabel": "1"
+        },
+        {
+            "id": "004",
+            "value": "42",
+            "showLabel": "1"
+        },
+        {
+            "id": "005",
+            "value": "36",
+            "showLabel": "1"
+        },
+        {
+            "id": "006",
+            "value": "12",
+            "showLabel": "1"
+        },
+        {
+            "id": "007",
+            "value": "75",
+            "showLabel": "1"
+        },
+        {
+            "id": "008",
+            "value": "8",
+            "showLabel": "1"
+        },
+        {
+            "id": "009",
+            "value": "97",
+            "showLabel": "1"
+        },
+        {
+            "id": "010",
+            "value": "36",
+            "showLabel": "1"
+        },
+        {
+            "id": "011",
+            "value": "80",
+            "showLabel": "1"
+        },
+        {
+            "id": "012",
+            "value": "41",
+            "showLabel": "1"
+        },
+        {
+            "id": "013",
+            "value": "72",
+            "showLabel": "1"
+        },
+        {
+            "id": "014",
+            "value": "78",
+            "showLabel": "1"
+        },
+        {
+            "id": "015",
+            "value": "41",
+            "showLabel": "1"
+        },
+        {
+            "id": "016",
+            "value": "87",
+            "showLabel": "1"
+        },
+        {
+            "id": "017",
+            "value": "2",
+            "showLabel": "1"
+        },
+        {
+            "id": "018",
+            "value": "79",
+            "showLabel": "1"
+        },
+        {
+            "id": "019",
+            "value": "97",
+            "showLabel": "1"
+        },
+        {
+            "id": "020",
+            "value": "60",
+            "showLabel": "1"
+        },
+        {
+            "id": "021",
+            "value": "6",
+            "showLabel": "1"
+        },
+        {
+            "id": "022",
+            "value": "26",
+            "showLabel": "1"
+        },
+        {
+            "id": "023",
+            "value": "96",
+            "showLabel": "1"
+        },
+        {
+            "id": "024",
+            "value": "32",
+            "showLabel": "1"
+        },
+        {
+            "id": "025",
+            "value": "56",
+            "showLabel": "1"
+        },
+        {
+            "id": "026",
+            "value": "34",
+            "showLabel": "1"
+        },
+        {
+            "id": "027",
+            "value": "35",
+            "showLabel": "1"
+        },
+        {
+            "id": "028",
+            "value": "90",
+            "showLabel": "1"
+        },
+        {
+            "id": "029",
+            "value": "15",
+            "showLabel": "1"
+        },
+        {
+            "id": "030",
+            "value": "42",
+            "showLabel": "1"
+        },
+        {
+            "id": "031",
+            "value": "11",
+            "showLabel": "1"
+        },
+        {
+            "id": "032",
+            "value": "46",
+            "showLabel": "1"
+        },
+        {
+            "id": "033",
+            "value": "14",
+            "showLabel": "1"
+        },
+        {
+            "id": "034",
+            "value": "68",
+            "showLabel": "1"
+        },
+        {
+            "id": "035",
+            "value": "14",
+            "showLabel": "1"
+        },
+        {
+            "id": "036",
+            "value": "84",
+            "showLabel": "1"
+        }
+    ]
+                }
+            });
+            annualPopulation.render();
+        });
+        </script>
 </head>
 
 
@@ -35,67 +261,20 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm">
-                <div id="chart"></div>
+                    <div id="chart-container">FusionMaps XT will load map here!</div>
             </div>
-            <div class="col-sm-4 section-filter">
+            <div class="col-sm-3 section-filter">
                 <div class="feature-box">
                     <i class="feature-box__icon icon-basic-compass"></i>
-                    <h3 class="heading-tertiary u-margin-bottom-small">Meet nature</h3>
+                    <h3 class="heading-tertiary u-margin-bottom-small">Rice Rating</h3>
                     <p class="feature-box__text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
+                        <!-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur. -->
                     </p>
                 </div>
             </div>
         </div>
     </div>
    </header>
-  
-  <script type="text/javascript">
-    var w = 600;
-    var h = 650;
-    var proj = d3.geo.mercator();
-    var path = d3.geo.path().projection(proj);
-    var t = proj.translate(); // the projection's default translation
-    var s = proj.scale() // the projection's default scale
-
-    var svg = d3.select("#chart")
-             .append("svg:svg")
-             .attr("width", w)
-        .attr("height", h)
-        .call(initialize);
-
-    var map = svg.append("svg:g")
-        
-
-    var india = map.append("svg:g")
-        .attr("id", "india");
-
-    
-
-    d3.json("lite/data/full_data-1.json", function (json) {
-      india.selectAll("path")
-          .data(json.features)
-        .enter().append("path")
-          .attr("d", path)
-          .attr("fill", "#000")
-          .append("title")
-          .text(function(d){
-          	return "district : " + d.properties.NAME_2 + " " + " state : " + d.properties.NAME_1;
-          });
-    });
-
-    
-    
-
-    function initialize() {
-      proj.scale(6700);
-      proj.translate([-1240, 750]);
-    }
-
-
-
-
-  </script>
 
 
 
