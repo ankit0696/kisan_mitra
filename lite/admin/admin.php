eddce730491d30ef361de1bac052c9af70b4b5ea
Ankit Kumar <ankitkr0696@gmail.com>
2019-02-26 00:23:13 +0530

    up

--- lite/admin/admin.php
+++ lite/admin/admin.php
@@ -33,7 +33,7 @@ if(!isset($_SESSION['email']))
       <li data-bind="css: {active: displayTab() === 'users'}, click: changeTab.bind($data, 'users')"><a><i class="fa fa-users"></i><span class="label">Users</span><i class="fa fa-caret-left caret"></i></a></li>
       <li data-bind="css: {active: displayTab() === 'documents'}, click: changeTab.bind($data, 'documents')"><a><i class="fa fa-archive"></i><span class="label">Documents</span><i class="fa fa-caret-left caret">  </i></a></li>
       <li data-bind="css: {active: displayTab() === 'pins'}, click: changeTab.bind($data, 'pins')"><a><i class="fa fa-thumb-tack"></i><span class="label">Pinned Docs</span><i class="fa fa-caret-left caret"></i></a></li>
-      <li class="nav-control"><a href="./api/logout.php"><i class="fa fa-sign-out"></i>Logout</a></li>
+      <li class="nav-control"><a href="./api/logout.php"><i class="fa fa-sign-out"></i><span class="label">Logout</span><i class="fa fa-caret-left caret"></i></a></li>
 
 
     </ul>
